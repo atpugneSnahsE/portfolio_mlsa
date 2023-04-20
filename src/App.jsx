4b7deda57e03dc1149e22c35c427f8577c9af383
EshanSengupta <eshansengupta2000@gmail.com>
2023-04-20 21:12:52 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Eshan Sengupta",
+  title: "Graphics Designer & ML Researcher",
+  email: "eshansengupta2000@gmail.com",
+  gitHub: "https://github.com/atpugneSnahsE",
+  instagram: "https://instagram.com/the.eshan.sengupta",
+  linkedIn: "https://www.linkedin.com/in/eshansengupta",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/_eshansengupta",
+  youTube: "https://www.youtube.com/channel/UC-mzo8KzciMzm5WeGHqB0YA",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,21 +10,21 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/mosaic.svg";
+import image from "../images/mosaic.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "orange red and gray pyraminds in abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a computer science student who spends most of his time explaining his tasks to machines";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "My passions include using specialized data and algorithms to solve real-world issues and explaining complex concepts to people. I can move quickly between industries to provide high-performing and Innovative approaches.";
 
 const About = () => {
   return (
